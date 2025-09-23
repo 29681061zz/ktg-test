c652231ee0b5a7d16d1e51fc596305877e7dd3aa
29681061zz <2968106122@qq.com>
2025-09-23 23:45:06 +0800

    基础设置更改

--- configs/settings.py
+++ configs/settings.py
@@ -7,8 +7,5 @@ class Config:
     HEADLESS =False     # 无头模式
     WINDOW_SIZE = "1920,1080"  #窗口大小
 
-    IMPLICIT_WAIT = 10  # 隐式等待时间
-    EXPLICIT_WAIT = 15  # 显式等待时间
-
     REPORT_PATH = "./reports"  # 测试报告路径
     REPORT_TITLE = "苦糖果MES系统自动化测试报告"  # 报告标题
