64addee44c3951d5dd44781e7d770769647ccf29
29681061zz <2968106122@qq.com>
2025-09-30 22:43:32 +0800

    新增arrivalnotice_driver

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -57,3 +57,11 @@ def warehouse_driver(logged_in_driver, request):
     target_url = f"{Config.BASE_URL}/mes/wm/warehouse"
     logged_in_driver.get(target_url)
     yield logged_in_driver
+
+@pytest.fixture(scope="function")
+def arrivalnotice_driver(logged_in_driver, request):
+    """仓库设置driver"""
+    request.node.driver = logged_in_driver
+    target_url = f"{Config.BASE_URL}/mes/wm/arrivalnotice"
+    logged_in_driver.get(target_url)
+    yield logged_in_driver
