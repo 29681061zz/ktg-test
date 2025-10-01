847be1c275273ece4d221d6cfa9738a1fe5bb6a6
29681061zz <2968106122@qq.com>
2025-10-01 21:27:02 +0800

    新增产品入库driver

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -73,3 +73,11 @@ def itemrecpt_driver(logged_in_driver, request):
     target_url = f"{Config.BASE_URL}/mes/wm/itemrecpt"
     logged_in_driver.get(target_url)
     yield logged_in_driver
+
+@pytest.fixture(scope="function")
+def productrecpt_driver(logged_in_driver, request):
+    """产品入库driver"""
+    request.node.driver = logged_in_driver
+    target_url = f"{Config.BASE_URL}/mes/wm/productrecpt"
+    logged_in_driver.get(target_url)
+    yield logged_in_driver
