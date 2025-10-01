d160527b0f2f3026be6eff96560d61327ee55b0c
29681061zz <2968106122@qq.com>
2025-10-01 17:49:56 +0800

    TestItemRecpt

---
+++ test_cases/ui_tests/test_warehouse_management/test_itemrecpt.py
@@ -0,0 +1,71 @@
+import pytest
+import allure
+from pages.warehouse_management_page.itemrecpt_page import ItemRecpt
+@pytest.fixture(scope="function")
+def itemrecpt_page(itemrecpt_driver):
+    """创建到货通知页面对象"""
+    return ItemRecpt(itemrecpt_driver)
+@allure.feature("到货通知")
+@pytest.mark.ui
+class TestItemRecpt:
+    """到货通知测试用例"""
+    @allure.story("新增功能")
+    @pytest.mark.parametrize("add_data,expected", [
+        ({
+            "code": "IRT_001",
+            "name": "新增到货通知_001",
+            "pocode": "POC_001",
+            "date": "2025-10-10",
+            "vendorname": "测试供应商_001",
+            "noticecode": "ANT_001",
+         }, True),
+    ])
+    def test_add_itemrecpt(self, itemrecpt_page, add_data, expected):
+        """测试到货通知新增功能 - 数据驱动"""
+        itemrecpt_page.add_itemrecpt(add_data)
+        # 搜索并验证新增的到货通知存在
+        itemrecpt_page.search_itemrecpt(add_data)
+        actual_result = itemrecpt_page.is_itemrecpt_exists(add_data)
+        assert actual_result == expected
+
+    @allure.story("搜索功能")
+    @pytest.mark.parametrize("search_data,expected", [
+        ({"code": "IRT_001"}, True),
+        ({"code": "IRT_000"}, False),
+        ({"name": "新增到货通知_001"}, True),
+        # ({"name": "不存在的到货通知"}, False),
+        # ({"code": "ANT_001","name": "新增到货通知_001"}, True),
+        # ({"code": "ANT_001", "name": "不存在的到货通知"}, False)
+    ])
+    def test_search_itemrecpt(self, itemrecpt_page, search_data, expected):
+        """测试到货通知搜索功能 - 数据驱动"""
+        itemrecpt_page.search_itemrecpt(search_data)
+        # 验证搜索结果
+        actual_result = itemrecpt_page.is_itemrecpt_exists(search_data)
+        assert actual_result == expected
+
+    @allure.story("修改功能")
+    @pytest.mark.parametrize("edit_data,expected", [
+        ({
+            "code" : "IRT_001",
+            "edit_name": "修改到货通知",
+            "pocode": "",
+            "date": "9999-10-10",
+            # "vendorname": "",
+         }, True),
+    ])
+    def test_edit_itemrecpt(self, itemrecpt_page, edit_data, expected):
+        """测试到货通知修改功能 - 数据驱动"""
+        itemrecpt_page.edit_itemrecpt(edit_data)
+        actual_result = itemrecpt_page.is_itemrecpt_exists(edit_data)
+        assert actual_result == expected
+
+    @allure.story("删除功能")
+    @pytest.mark.parametrize("delete_data,expected", [
+        ({"code" : "IRT_001",}, False), # 期望删除后到货通知不存在
+    ])
+    def test_delete_itemrecpt(self, itemrecpt_page, delete_data, expected):
+        """测试到货通知删除功能 - 数据驱动"""
+        itemrecpt_page.delete_itemrecpt(delete_data)
+        actual_result = itemrecpt_page.is_itemrecpt_exists(delete_data)
+        assert actual_result == expected
