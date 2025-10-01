61100c8b53da88568221a662c537166b16a3989a
29681061zz <2968106122@qq.com>
2025-10-01 17:47:02 +0800

    新增采购入库driver

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -60,8 +60,16 @@ def warehouse_driver(logged_in_driver, request):
 
 @pytest.fixture(scope="function")
 def arrivalnotice_driver(logged_in_driver, request):
-    """仓库设置driver"""
+    """到货通知driver"""
     request.node.driver = logged_in_driver
     target_url = f"{Config.BASE_URL}/mes/wm/arrivalnotice"
+    logged_in_driver.get(target_url)
+    yield logged_in_driver
+
+@pytest.fixture(scope="function")
+def itemrecpt_driver(logged_in_driver, request):
+    """采购入库driver"""
+    request.node.driver = logged_in_driver
+    target_url = f"{Config.BASE_URL}/mes/wm/itemrecpt"
     logged_in_driver.get(target_url)
     yield logged_in_driver
