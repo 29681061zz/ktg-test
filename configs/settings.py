fe88bc0a31e41a4168eee3597dc9d82e66bf409f
29681061zz <2968106122@qq.com>
2025-10-07 18:08:24 +0800

    基础配置

--- configs/settings.py
+++ configs/settings.py
@@ -1,11 +1,8 @@
+import os
 
 class Config:
     BASE_URL ="http://www.029tec.com"
-    DASHBOARD_URL = BASE_URL + "/index"
-
-    BROWSER = "edge"  # 浏览器类型: chrome, firefox, edge
-    HEADLESS =False     # 无头模式
-    WINDOW_SIZE = "1920,1080"  #窗口大小
-
-    REPORT_PATH = "./reports"  # 测试报告路径
-    REPORT_TITLE = "苦糖果MES系统自动化测试报告"  # 报告标题
+    API_BASE_URL = BASE_URL
+    TEST_USERNAME = os.getenv("MES_USERNAME", "xhran")
+    TEST_PASSWORD = os.getenv("MES_PASSWORD", "123456")
+    API_TIMEOUT = 10
