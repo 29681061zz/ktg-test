83d7bcf6aef77ad3cdb7ed3962ef79cccdbaa20d
29681061zz <2968106122@qq.com>
2025-10-07 18:10:54 +0800

    test_cases\ui_tests\test_master_data\conftest.py

--- test_cases/ui_tests/test_master_data/conftest.py
+++ test_cases/ui_tests/test_master_data/conftest.py
@@ -49,35 +49,3 @@ def workstation_driver(logged_in_driver, request):
     target_url = f"{Config.BASE_URL}/mes/md/workstation"
     logged_in_driver.get(target_url)
     yield logged_in_driver
-
-@pytest.fixture(scope="function")
-def warehouse_driver(logged_in_driver, request):
-    """仓库设置driver"""
-    request.node.driver = logged_in_driver
-    target_url = f"{Config.BASE_URL}/mes/wm/warehouse"
-    logged_in_driver.get(target_url)
-    yield logged_in_driver
-
-@pytest.fixture(scope="function")
-def arrivalnotice_driver(logged_in_driver, request):
-    """到货通知driver"""
-    request.node.driver = logged_in_driver
-    target_url = f"{Config.BASE_URL}/mes/wm/arrivalnotice"
-    logged_in_driver.get(target_url)
-    yield logged_in_driver
-
-@pytest.fixture(scope="function")
-def itemrecpt_driver(logged_in_driver, request):
-    """采购入库driver"""
-    request.node.driver = logged_in_driver
-    target_url = f"{Config.BASE_URL}/mes/wm/itemrecpt"
-    logged_in_driver.get(target_url)
-    yield logged_in_driver
-
-@pytest.fixture(scope="function")
-def productrecpt_driver(logged_in_driver, request):
-    """产品入库driver"""
-    request.node.driver = logged_in_driver
-    target_url = f"{Config.BASE_URL}/mes/wm/productrecpt"
-    logged_in_driver.get(target_url)
-    yield logged_in_driver
