9491bc28d06bf07333365aef8f6288549abe3503
29681061zz <2968106122@qq.com>
2025-10-09 20:36:16 +0800

    测试conftest

--- test_cases/api_tests/test_production_management/conftest.py
+++ test_cases/api_tests/test_production_management/conftest.py
@@ -1,9 +1,25 @@
 import pytest
 
 @pytest.fixture(scope="function")
+def authenticated_workorder_api(api_token):
+    """已认证的生产工单API - 函数式fixture"""
+    from apis.production_management_api.workorder_api import WorkOrderAPI
+    api = WorkOrderAPI()
+    api.set_auth_token(api_token)
+    yield api
+
+@pytest.fixture(scope="function")
 def authenticated_process_api(api_token):
     """已认证的工序API - 函数式fixture"""
     from apis.production_management_api.process_api import ProcessAPI
     api = ProcessAPI()
+    api.set_auth_token(api_token)
+    yield api
+
+@pytest.fixture(scope="function")
+def authenticated_proroute_api(api_token):
+    """已认证的工艺路线API - 函数式fixture"""
+    from apis.production_management_api.proroute_api import ProrouteAPI
+    api = ProrouteAPI()
     api.set_auth_token(api_token)
     yield api
