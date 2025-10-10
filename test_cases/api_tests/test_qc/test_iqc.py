712f0696e923dfca5d56c58dafbe12e0e82f1c7f
29681061zz <2968106122@qq.com>
2025-10-10 16:53:04 +0800

    test_iqc.py

---
+++ test_cases/api_tests/test_qc/test_iqc.py
@@ -0,0 +1,34 @@
+import allure
+import pytest
+from utils.assertion import ApiAssertion
+from utils.data_manager import DataManager
+
+@pytest.mark.api
+@allure.feature("检测项设置")
+class TestIqc:
+    """检测项设置测试 - 数据驱动"""
+    @allure.story("新增功能")
+    @DataManager.qc('iqc', 'add_cases', ['add_data', 'expected_status'])
+    def test_add_iqc(self, authenticated_iqc_api,add_data, expected_status):
+        # 创建检测项
+        response = authenticated_iqc_api.add_iqc(add_data)
+        # 检查API调用成功
+        (ApiAssertion.assert_status_code(response['status_code'],200)
+         .assert_business_code(response['raw']['code'], expected_status))
+        # 只有业务成功的情况下才进行搜索验证
+        if response['raw']['code'] == 200:  # 业务成功
+            # 重新搜索验证数据创建
+            search_response = authenticated_iqc_api.search_iqc(add_data)
+            # 检查搜索到数据且字段按测试数据全匹配
+            (ApiAssertion.assert_all_fields_match(search_response['data'][0], add_data))
+
+    @allure.story("搜索功能")
+    @DataManager.qc('iqc', 'search_cases', ['search_data', 'expected_status'])
+    def test_search_iqc(self, authenticated_iqc_api,search_data, expected_status):
+        """测试检测项搜索"""
+        response = authenticated_iqc_api.search_iqc(search_data)
+        (ApiAssertion.assert_status_code(response['status_code'], 200)
+         .assert_business_code(response['raw']['code'], expected_status)
+         .assert_json_contains(response, 'data')
+         .assert_all_fields_match(response['data'][0], search_data))
+
