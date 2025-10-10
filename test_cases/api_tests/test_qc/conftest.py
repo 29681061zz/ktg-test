879eced1671f44046bd93f502d16c2b702d716bf
29681061zz <2968106122@qq.com>
2025-10-10 16:53:25 +0800

    test_cases\api_tests\test_qc\conftest.py

--- test_cases/api_tests/test_qc/conftest.py
+++ test_cases/api_tests/test_qc/conftest.py
@@ -7,3 +7,27 @@ def authenticated_qcdefect_api(api_token):
     api = QcdefectAPI()
     api.set_auth_token(api_token)
     yield api
+
+@pytest.fixture(scope="function")
+def authenticated_qcindex_api(api_token):
+    """已认证的常见缺陷API - 函数式fixture"""
+    from apis.qc_api.qcindex_api import QcindexAPI
+    api = QcindexAPI()
+    api.set_auth_token(api_token)
+    yield api
+
+@pytest.fixture(scope="function")
+def authenticated_qctemplate_api(api_token):
+    """已认证的常见缺陷API - 函数式fixture"""
+    from apis.qc_api.qctemplate_api import QctemplateAPI
+    api = QctemplateAPI()
+    api.set_auth_token(api_token)
+    yield api
+
+@pytest.fixture(scope="function")
+def authenticated_iqc_api(api_token):
+    """已认证的常见缺陷API - 函数式fixture"""
+    from apis.qc_api.iqc_api import IqcAPI
+    api = IqcAPI()
+    api.set_auth_token(api_token)
+    yield api
