99f642da6441234e57b98d519795f3194ad9d3a1
29681061zz <2968106122@qq.com>
2025-10-10 22:54:37 +0800

    test_cases\api_tests\test_device_management\conftest.py

---
+++ test_cases/api_tests/test_device_management/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+@pytest.fixture(scope="function")
+def authenticated_machinerytype_api(api_token):
+    """已认证的设备类型设置API - 函数式fixture"""
+    from apis.device_management_api.machinerytype_api import MachineryTypeAPI
+    api = MachineryTypeAPI()
+    api.set_auth_token(api_token)
+    yield api
+
+@pytest.fixture(scope="function")
+def authenticated_machinery_api(api_token):
+    """已认证的设备类型设置API - 函数式fixture"""
+    from apis.device_management_api.machinery_api import MachineryAPI
+    api = MachineryAPI()
+    api.set_auth_token(api_token)
+    yield api
+
+@pytest.fixture(scope="function")
+def authenticated_dvsubject_api(api_token):
+    """已认证的设备类型设置API - 函数式fixture"""
+    from apis.device_management_api.dvsubject_api import DvsubjectAPI
+    api = DvsubjectAPI()
+    api.set_auth_token(api_token)
+    yield api
