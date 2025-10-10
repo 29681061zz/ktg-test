2821279bf846caaed7d8d05b4b6ed634c51e33aa
29681061zz <2968106122@qq.com>
2025-10-10 22:54:04 +0800

    settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -3,6 +3,6 @@ import os
 class Config:
     BASE_URL ="http://www.029tec.com"
     API_BASE_URL = BASE_URL
-    TEST_USERNAME = os.getenv("MES_USERNAME", "xhran")
+    TEST_USERNAME = os.getenv("MES_USERNAME", "testuser")
     TEST_PASSWORD = os.getenv("MES_PASSWORD", "123456")
     API_TIMEOUT = 10
