5fe45cd080848501be080b962364121dc0a0c028
29681061zz <2968106122@qq.com>
2025-10-10 22:54:19 +0800

    test_machinery.py

---
+++ test_cases/api_tests/test_device_management/test_machinery.py
@@ -0,0 +1,64 @@
+import allure
+import pytest
+from utils.assertion import ApiAssertion
+from utils.data_manager import DataManager
+
+@pytest.mark.api
+@allure.feature("设备台账")
+class TestMachinery:
+    """设备台账测试 - 数据驱动"""
+    @allure.story("新增功能")
+    @DataManager.device('machinery', 'add_cases', ['add_data', 'expected_status'])
+    def test_add_machinery(self, authenticated_machinery_api,add_data, expected_status):
+        # 创建设备
+        response = authenticated_machinery_api.add_machinery(add_data)
+        # 检查API调用成功
+        (ApiAssertion.assert_status_code(response['status_code'],200)
+         .assert_business_code(response['raw']['code'], expected_status))
+        # 只有业务成功的情况下才进行搜索验证
+        if response['raw']['code'] == 200:  # 业务成功
+            # 重新搜索验证数据创建
+            search_response = authenticated_machinery_api.search_machinery(add_data)
+            # 检查搜索到数据且字段按测试数据全匹配
+            (ApiAssertion.assert_all_fields_match(search_response['data'][0], add_data))
+
+    @allure.story("搜索功能")
+    @DataManager.device('machinery', 'search_cases', ['search_data', 'expected_status'])
+    def test_search_machinery(self, authenticated_machinery_api,search_data, expected_status):
+        """测试设备搜索"""
+        response = authenticated_machinery_api.search_machinery(search_data)
+        (ApiAssertion.assert_status_code(response['status_code'], 200)
+         .assert_business_code(response['raw']['code'], expected_status)
+         .assert_json_contains(response, 'data')
+         .assert_all_fields_match(response['data'][0], search_data))
+
+    @allure.story("修改功能")
+    @DataManager.device('machinery', 'edit_cases', ['edit_data', 'expected_status'])
+    def test_edit_machinery(self, authenticated_machinery_api,edit_data, expected_status):
+        # 修改设备
+        response = authenticated_machinery_api.edit_machinery(edit_data)
+        (ApiAssertion.assert_status_code(response['status_code'], 200)
+         .assert_business_code(response['raw']['code'], expected_status))
+        if response['raw']['code'] == 200:  # 业务成功
+            # 重新搜索验证数据创建
+            search_response = authenticated_machinery_api.search_machinery(edit_data)
+            # 检查搜索到数据且字段按测试数据全匹配
+            (ApiAssertion.assert_all_fields_match(search_response['data'][0], edit_data))
+
+    @allure.story("删除功能")
+    @DataManager.device('machinery', 'delete_cases', ['delete_data', 'expected_status'])
+    def test_delete_machinery(self, authenticated_machinery_api, delete_data, expected_status):
+        """测试删除设备"""
+        # 先确保设备存在
+        search_response = authenticated_machinery_api.search_machinery(delete_data)
+        assert len(search_response['data']) > 0, "删除前设备应该存在"
+        # 删除设备
+        response = authenticated_machinery_api.delete_machinery(delete_data)
+        # 检查删除操作成功
+        (ApiAssertion.assert_status_code(response['status_code'], 200)
+         .assert_business_code(response['data']['code'], expected_status)
+         .assert_json_contains(response, 'data'))
+        # 验证设备确实被删除
+        if response['raw']['code'] == 200:
+            post_search = authenticated_machinery_api.search_machinery(delete_data)
+            assert len(post_search['data']) == 0, "删除后设备应该不存在"
