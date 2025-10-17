212a3839a3cf6dccefe285cd2fc21c7580029bcc
29681061zz <2968106122@qq.com>
2025-10-17 23:48:45 +0800

    test_iqc.py

--- test_cases/api_tests/test_qc/test_iqc.py
+++ test_cases/api_tests/test_qc/test_iqc.py
@@ -4,9 +4,9 @@ from utils.assertion import ApiAssertion
 from utils.data_manager import DataManager
 
 @pytest.mark.api
-@allure.feature("检测项设置")
+@allure.feature("来料检验")
 class TestIqc:
-    """检测项设置测试 - 数据驱动"""
+    """来料检验测试 - 数据驱动"""
     @allure.story("新增功能")
     @DataManager.qc('iqc', 'add_cases', ['add_data', 'expected_status'])
     def test_add_iqc(self, authenticated_iqc_api,add_data, expected_status):
@@ -29,6 +29,7 @@ class TestIqc:
         response = authenticated_iqc_api.search_iqc(search_data)
         (ApiAssertion.assert_status_code(response['status_code'], 200)
          .assert_business_code(response['raw']['code'], expected_status)
-         .assert_json_contains(response, 'data')
-         .assert_all_fields_match(response['data'][0], search_data))
+         .assert_json_contains(response, 'data'))
+        if response['data']:  # 只有存在数据时才进行字段匹配断言
+            ApiAssertion.assert_all_fields_match(response['data'][0], search_data)
 
