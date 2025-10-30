57a9ab4f03bb89060e3d714b7cc1052afe3b0c71
29681061zz <2968106122@qq.com>
2025-10-31 00:35:14 +0800

    machinery.json

--- apis/device_management_api/machinery_api.py
+++ apis/device_management_api/machinery_api.py
@@ -2,15 +2,15 @@ from typing import Dict, Any
 from apis.base_api import BaseApi
 
 class MachineryAPI(BaseApi):
-    """检测项设置API客户端"""
+    """设备台账API客户端"""
     def __init__(self, base_url: str = None):
-        """初始化检测项设置API客户端"""
+        """初始化设备台账API客户端"""
         if base_url is None:
             base_url = "/prod-api"
         super().__init__(base_url)
 
     def search_machinery(self,search_data:Dict[str, Any]) -> Dict[str, Any]:
-        """搜索检测项"""
+        """搜索设备台账"""
         endpoint = "/mes/dv/machinery/list"
         return self.client.get(endpoint, params=search_data)
 
@@ -19,16 +19,16 @@ class MachineryAPI(BaseApi):
         return self.client.post(endpoint, json_data=edit_data)
 
     def edit_machinery(self,edit_data: Dict[str, Any]) -> Dict[str, Any]:
-        """修改检测项信息"""
+        """修改设备台账信息"""
         endpoint = "/mes/dv/machinery"
         search_response = self.search_machinery({"machineryCode": edit_data["machineryCode"]})
         edit_data["machineryId"]= search_response['data'][0]['machineryId']
         return self.client.put(endpoint, json_data=edit_data)
 
     def delete_machinery(self, delete_data: Dict[str, Any]) -> Dict[str, Any]:
-        """删除检测项"""
+        """删除设备台账"""
         endpoint = "/mes/dv/machinery"
-        # 通过编码搜索获取检测项ID
+        # 通过编码搜索获取设备台账ID
         search_response = self.search_machinery(delete_data)
         machinery_id = search_response['data'][0]['machineryId']
         # 发送删除请求
